eb5a3b0ebb4ae60f57979d31cbbf2f55fefe23fe
user9 <module6user9@gmail.com>
2024-06-10 13:38:22 +0200

    Добавьте файлы проекта.

---
+++ Kursov.Rabota.Kruchinina.23-isp-2-1/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Kursov.Rabota.Kruchinina._23_isp_2_1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
